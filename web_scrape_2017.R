b25881627f1e5367a90aeb4af1e371c2aeea1fdb
Fabian <fabiandagnone@gmail.com>
2018-02-04 20:27:55 +0800

    working all teams - output numeric

--- web_scrape_2017.R
+++ web_scrape_2017.R
@@ -5,14 +5,17 @@ library(reshape2)
 
 setwd("/Users/fabiand/Documents/GitHub/football_fun/")
 
+#names of teams - used to label datasets and create variable "team"
 teams <- c("adelaide","brisbane","carlton","collingwood","essendon","fremantle","geelong","goldcoast",
-            "gws","hawthorn","melbourne","north","port","richdmond","stkilda","sydney","westcoast","bulldogs")
+            "gws","hawthorn","melbourne","north melbourne","port adelaide","richmond","stkilda","sydney","westcoast","bulldogs")
+
+# the strings used in the website address, note some times have differences i.e. brisbane and bulldogs
+site_teams <- c("adelaide","brisbanel","carlton","collingwood","essendon","fremantle","geelong","goldcoast",
+            "gws","hawthorn","melbourne","kangaroos","padelaide","richmond","stkilda","swans","westcoast","bullldogs")
 
 statistics <- c("disposals","kicks","marks","handballs","goals","behinds","hitouts","tackles","rebounds","inside50",
                 "clearances","clangers","ff","fa","brownlow","con_pos","uncon_pos","con_mark","mark_in50","oneper",
                 "bounces","ga","play")
-
-
 
 #### Function required to append many datasets from a list
 AppendMe <- function(dfNames) {
@@ -25,31 +28,38 @@ AppendMe <- function(dfNames) {
 # year that data will be scrapped
 year=2017
 
-#### team level of the forloop
+#### team level of the for loop
 for(j in 1:18){
 
-  url1 <- paste0("https://afltables.com/afl/stats/teams/",teams[j],"/",year,"_gbg.html"
+  url1 <- paste0("https://afltables.com/afl/stats/teams/",site_teams[j],"/",year,"_gbg.html")
   URL <- getURL(url1)
 
   for(i in 1:23){
 
     x<-readHTMLTable(URL,which=i)
     xx <- melt(x,id="Player")
-    xx <- xx[xx$value != "",]
+  #  xx <- xx[!is.na(xx$value),]
+    xx <- xx[xx$value != '',]
+
     colnames(xx)[2:3] <- c("round",statistics[i])
 
     if(i == 1){
       assign(teams[j], xx)
       } else{
         assign(teams[j],merge(x=get(teams[j]),y=xx,by=c("Player","round")))
-        }
+      }
 
     assign(teams[j], `[[<-`(get(teams[j]), 'team', value = teams[j]))
+
   }
 
-  afl <- AppendMe(teams)
+  cat(paste0(teams[j],"-DONE \n"))
 
 }
 
+## use function to append all teams data together
+afl <- AppendMe(teams)
+
+
 #Output
-save(afl,file=paste0("afl",year,".RData")
+save(afl,file=paste0("afl",year,".RData"))
